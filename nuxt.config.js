f259ee705d95ea61b9da1df2eeb1fc4ccb664f7c
dommi10 <domsbuhendwa2@gmail.com>
2022-03-25 22:23:34 +0300

    fix bugs

--- nuxt.config.js
+++ nuxt.config.js
@@ -14,7 +14,7 @@ export default {
   // Global CSS: https://go.nuxtjs.dev/config-css
   css: [],
 
-  serverMiddleware: [{ path: "/api", handler: "~/server-middleware/app.js" }],
+  serverMiddleware: [{ path: "/api", handler: "~/api/app.js" }],
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [],
 
