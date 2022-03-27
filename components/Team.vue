1b131030d8e72bafe12f623d0e10ad0d9e005d2d
dommi10 <domsbuhendwa2@gmail.com>
2022-03-27 21:51:59 +0300

    fix deploy

--- components/Team.vue
+++ components/Team.vue
@@ -21,7 +21,7 @@
           :imageUrl="
             item.imageUrl
               ? item.imageUrl
-              : 'https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80'
+              : 'https://images.unsplash.com/photo-1634926878768-2a5b3c42f139?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=456&q=80'
           "
         />
       </div>
